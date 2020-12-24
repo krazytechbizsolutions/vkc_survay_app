fdb7090d761f55ad53f2f845c8dfaad7d6d9c5dd
raj-patel-sys <patelrajj1904@gmail.com>
2020-12-24 14:03:51 +0530

    ngrok and eslint config

--- src/screens/SurveyQue/index.js
+++ src/screens/SurveyQue/index.js
@@ -1,14 +1,27 @@
 /* eslint-disable */
 import React from 'react';
 import useSWR from 'swr';
-import { Button, SafeAreaView } from 'react-native';
+import { Button, SafeAreaView, Text } from 'react-native';
 
 const SurveyQue = ({ navigation, route }) => {
   const { questionId } = route.params;
   const { data } = useSWR(`/questions/${questionId}`);
 
+  if (!data) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <Choose>
+        <When condition={data.questionType === 'singleSelectRadio'}>
+          <Text>{data.question}</Text>
+        </When>
+        <When condition={data.questionType === 'singleSelectList'}></When>
+        <Otherwise>
+          <Text>ElseBlock</Text>
+        </Otherwise>
+      </Choose>
       <Button
         title="next"
         onPress={() =>
