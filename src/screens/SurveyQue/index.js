356f75d6305290c9a54e8970fa46963ce8c8b551
raj-patel-sys <patelrajj1904@gmail.com>
2020-12-24 16:04:20 +0530

    changes is there

--- src/screens/SurveyQue/index.js
+++ src/screens/SurveyQue/index.js
@@ -1,4 +1,6 @@
-/* eslint-disable */
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import useSWR from 'swr';
 import { Button, SafeAreaView, Text, ScrollView, View } from 'react-native';
@@ -33,20 +35,20 @@ const SurveyQue = ({ navigation, route }) => {
         <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
           <View style={{ flex: 1 }}>
             <Button
-              title="next"
+              title="Previous"
               onPress={() =>
                 navigation.navigate('SurveyQue', {
-                  questionId: parseInt(questionId, 10) + 1,
+                  questionId: parseInt(questionId, 10) - 1,
                 })
               }
             />
           </View>
           <View style={{ flex: 1 }}>
             <Button
-              title="Previous"
+              title="next"
               onPress={() =>
                 navigation.navigate('SurveyQue', {
-                  questionId: parseInt(questionId, 10) - 1,
+                  questionId: parseInt(questionId, 10) + 1,
                 })
               }
             />
