c0780645145e39cffbc8871402adcde3fdffd891
raj-patel-sys <patelrajj1904@gmail.com>
2020-12-24 15:02:45 +0530

    multiSelect done

--- src/components/CheckBoxField/CheckBoxField.js
+++ src/components/CheckBoxField/CheckBoxField.js
@@ -1,8 +1,26 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { BorderlessButton } from 'react-native-gesture-handler';
+import { Text, View } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import Checkbox from '../CheckBox/CheckBox';
+import CheckboxIcon from '../../assets/icons/check_box.svg';
+import CheckboxOutline from '../../assets/icons/check_box_outline.svg';
+
+export const CheckBoxFill = ({ option, value, onPress }) => (
+  <RectButton onPress={onPress}>
+    <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
+      {value !== '' && value.includes(option.value) ? (
+        <CheckboxIcon width={24} height={24} fill="blue" />
+      ) : (
+        <CheckboxOutline width={24} height={24} fill="blue" />
+      )}
+      <Text variant="label" style={{ marginHorizontal: 10 }}>
+        {option.text}
+      </Text>
+    </View>
+  </RectButton>
+);
 
 const CheckboxField = ({
   field: { name, value },
--- src/components/SingleSelectList/index.js
+++ src/components/SingleSelectList/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+/* eslint-disable react/prop-types */
+import TextEle from '@components/TextEle';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { CheckBoxFill } from '@components/CheckBoxField/CheckBoxField';
 
-const SingleSelectList = () => (
-  <View>
-    <Text>Single Selet List</Text>
-  </View>
-);
+const SingleSelectList = ({ data }) => {
+  const [value, setValue] = useState([]);
+  return (
+    <View>
+      <TextEle variant="title">{data.question}</TextEle>
+      <For each="item" of={data.options}>
+        <CheckBoxFill
+          option={{ text: item.text, value: item.text }}
+          value={value}
+          onPress={() => {
+            if (value.some(ele => ele === item.text)) {
+              setValue(value.filter(x => x !== item.text));
+            } else {
+              setValue([...value, item.text]);
+            }
+          }}
+        />
+      </For>
+    </View>
+  );
+};
 
 export default SingleSelectList;
