c428fdc0e84d5763ac9fa9d41a2579440a974ff2
imyagnesh <yagnesh.modh@gmail.com>
2021-02-04 15:55:26 +0530

    comit code

--- src/screens/SurveyQue/index.js
+++ src/screens/SurveyQue/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-curly-newline */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { SafeAreaView, Text, ScrollView, View, Alert } from 'react-native';
 // import { useTheme } from '@react-navigation/native';
 import VKCButton from '@components/VKCButton';
@@ -15,9 +15,9 @@ import SelectGroup from '@components/SelectGroup';
 // import IntegerInput from '@components/IntegerInput';
 import SliderQuestion from '@components/SliderQuestion';
 import StarRating from '@components/StarRating';
-import TextEle from '@components/TextEle';
 import { TextInput } from 'react-native-gesture-handler';
 import MultiText from '@components/MultiText';
+import { Formik, Field } from 'formik';
 // import TextInput from '@components/TextInput/TextInput';
 // import { RectButton } from 'react-native-gesture-handler';
 // import SubmitServey from '../../components/SubmitServey';
@@ -26,143 +26,202 @@ const SurveyQue = ({ navigation, route }) => {
   // const { colors } = useTheme();
   const { questions, firstQuestion } = route.params;
   const [question, ...restQuestions] = questions;
-  const [value, setValue] = useState('');
+  const formRef = useRef();
+
+  const onSubmit = values => {
+    console.log(values);
+    if (restQuestions.length === 0) {
+      Alert.alert(
+        'Completed',
+        'Form Submition Completed',
+        [{ text: 'OK', onPress: () => navigation.popToTop() }],
+        { cancelable: false },
+      );
+    } else {
+      navigation.push('SurveyQue', {
+        questions: restQuestions,
+        firstQuestion: false,
+      });
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, margin: 10 }}>
       <ScrollView style={{ flex: 1 }}>
-        <Choose>
-          <When condition={question.sQuestion.Option_Type__c === 'Single Select'}>
-            <SingleSelectRadio
-              value={value}
-              onSelect={item => setValue(item)}
-              data={question.Options}
-              valueField="optionId"
-              textField="optionName"
-              question={question.sQuestion.Detailed_Survey_Question_Name__c}
-            />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Single Select List'}>
-            <SingleSelectList
-              data={question.Options.map(x => ({
-                text: x.optionName,
-                value: x.optionId,
-              }))}
-            />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Single Select Group'}>
-            <SelectGroup data={question.Options} />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Multi Select'}>
-            <MultiSelection
-              data={question.Options.map(x => ({
-                text: x.optionName,
-                value: x.optionId,
-              }))}
-            />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Ordering Question'}>
-            <VKCDraggableList data={question.Options} />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Integer Enter Question'}>
-            <TextInput
-              style={{
-                height: 56,
-                paddingHorizontal: 24,
-                borderColor: 'black',
-                borderWidth: 2,
-                borderRadius: 32,
-                fontWeight: '500',
-                fontFamily: 'Inter-Medium',
-                fontSize: 15,
-                fontStyle: 'normal',
-                lineHeight: 18,
-                letterSpacing: 0.7,
-                textAlign: 'left',
-                marginVertical: 10,
-              }}
-              keyboardType="numeric"
-            />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Text'}>
-            <TextInput
-              style={{
-                height: 56,
-                paddingHorizontal: 24,
-                borderColor: 'black',
-                borderWidth: 2,
-                borderRadius: 32,
-                fontWeight: '500',
-                fontFamily: 'Inter-Medium',
-                fontSize: 15,
-                fontStyle: 'normal',
-                lineHeight: 18,
-                letterSpacing: 0.7,
-                textAlign: 'left',
-                marginVertical: 10,
-              }}
-            />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Slider'}>
-            <SliderQuestion data={question.sQuestion} />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Star Rating'}>
-            <StarRating data={question.sQuestion} />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Question with Image as options'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <When
-            condition={question.sQuestion.Option_Type__c === 'Upload Image for choosing an Option'}>
-            <VKCMediaPicker />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Multi Text'}>
-            <MultiText />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Feedback'}>
-            <TextInput
-              multiline
-              style={{
-                paddingHorizontal: 24,
-                borderColor: 'black',
-                borderWidth: 2,
-                borderRadius: 10,
-                fontWeight: '500',
-                fontFamily: 'Inter-Medium',
-                fontSize: 15,
-                fontStyle: 'normal',
-                lineHeight: 18,
-                letterSpacing: 0.7,
-                textAlign: 'left',
-                marginVertical: 10,
-              }}
-            />
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Tabular Question'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Coupon'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Display'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Stock'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Performance In the Area'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Salesman Commit'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <When condition={question.sQuestion.Option_Type__c === 'Special Efforts'}>
-            <Text>{question.sQuestion.Option_Type__c}</Text>
-          </When>
-          <Otherwise>
-            <Text>No Such Question type found</Text>
-          </Otherwise>
-        </Choose>
+        <Formik
+          innerRef={formRef}
+          initialValues={{
+            [question.sQuestion.Id]: '',
+            childField: '',
+          }}
+          onSubmit={onSubmit}>
+          {({ values }) => (
+            <Choose>
+              <When condition={question.sQuestion.Option_Type__c === 'Single Select'}>
+                <Field
+                  name={question.sQuestion.Id}
+                  component={SingleSelectRadio}
+                  data={question.Options}
+                  value={values[question.sQuestion.Id]}
+                  valueField="optionId"
+                  textField="optionName"
+                  question={question.sQuestion.Detailed_Survey_Question_Name__c}
+                  validate={value => {
+                    console.log(value);
+                    if (!value) {
+                      return 'Please Enter Field Value';
+                    }
+                    return '';
+                  }}
+                />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Single Select List'}>
+                <Field
+                  name={question.sQuestion.Id}
+                  component={SingleSelectRadio}
+                  data={question.Options}
+                  value={values[question.sQuestion.Id]}
+                  valueField="optionId"
+                  textField="optionName"
+                  question={question.sQuestion.Detailed_Survey_Question_Name__c}
+                  validate={value => {
+                    console.log(value);
+                    if (!value) {
+                      return 'Please Enter Field Value';
+                    }
+                    return '';
+                  }}
+                />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Single Select Group'}>
+                <SelectGroup data={question.Options} />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Multi Select'}>
+                <MultiSelection
+                  data={question.Options.map(x => ({
+                    text: x.optionName,
+                    value: x.optionId,
+                  }))}
+                />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Ordering Question'}>
+                <VKCDraggableList data={question.Options} />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Integer Enter Question'}>
+                <TextInput
+                  style={{
+                    height: 56,
+                    paddingHorizontal: 24,
+                    borderColor: 'black',
+                    borderWidth: 2,
+                    borderRadius: 32,
+                    fontWeight: '500',
+                    fontFamily: 'Inter-Medium',
+                    fontSize: 15,
+                    fontStyle: 'normal',
+                    lineHeight: 18,
+                    letterSpacing: 0.7,
+                    textAlign: 'left',
+                    marginVertical: 10,
+                  }}
+                  keyboardType="numeric"
+                />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Text'}>
+                <TextInput
+                  style={{
+                    height: 56,
+                    paddingHorizontal: 24,
+                    borderColor: 'black',
+                    borderWidth: 2,
+                    borderRadius: 32,
+                    fontWeight: '500',
+                    fontFamily: 'Inter-Medium',
+                    fontSize: 15,
+                    fontStyle: 'normal',
+                    lineHeight: 18,
+                    letterSpacing: 0.7,
+                    textAlign: 'left',
+                    marginVertical: 10,
+                  }}
+                />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Slider'}>
+                <SliderQuestion data={question.sQuestion} />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Star Rating'}>
+                <Field
+                  name={question.sQuestion.Id}
+                  value={values[question.sQuestion.Id]}
+                  component={StarRating}
+                  data={question.sQuestion}
+                  validate={value => {
+                    if (!value) {
+                      return 'Please Enter Field Value';
+                    }
+                    return '';
+                  }}
+                />
+              </When>
+              <When
+                condition={question.sQuestion.Option_Type__c === 'Question with Image as options'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <When
+                condition={
+                  question.sQuestion.Option_Type__c === 'Upload Image for choosing an Option'
+                }>
+                <VKCMediaPicker />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Multi Text'}>
+                <MultiText />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Feedback'}>
+                <TextInput
+                  multiline
+                  style={{
+                    paddingHorizontal: 24,
+                    borderColor: 'black',
+                    borderWidth: 2,
+                    borderRadius: 10,
+                    fontWeight: '500',
+                    fontFamily: 'Inter-Medium',
+                    fontSize: 15,
+                    fontStyle: 'normal',
+                    lineHeight: 18,
+                    letterSpacing: 0.7,
+                    textAlign: 'left',
+                    marginVertical: 10,
+                  }}
+                />
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Tabular Question'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Coupon'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Display'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Stock'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Performance In the Area'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Salesman Commit'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <When condition={question.sQuestion.Option_Type__c === 'Special Efforts'}>
+                <Text>{question.sQuestion.Option_Type__c}</Text>
+              </When>
+              <Otherwise>
+                <Text>No Such Question type found</Text>
+              </Otherwise>
+            </Choose>
+          )}
+        </Formik>
       </ScrollView>
       <Choose>
         <When condition={firstQuestion}>
@@ -170,12 +229,7 @@ const SurveyQue = ({ navigation, route }) => {
             variant="fill"
             style={{ marginVertical: 5 }}
             text="Next"
-            onPress={() =>
-              navigation.push('SurveyQue', {
-                questions: restQuestions,
-                firstQuestion: false,
-              })
-            }
+            onPress={() => formRef.current.handleSubmit()}
           />
         </When>
         <Otherwise>
@@ -190,19 +244,7 @@ const SurveyQue = ({ navigation, route }) => {
               variant="fill"
               style={{ margin: 5, flex: 1 }}
               text={restQuestions.length === 0 ? 'Submit' : 'Next'}
-              onPress={() =>
-                restQuestions.length === 0
-                  ? Alert.alert(
-                      'Completed',
-                      'Form Submition Completed',
-                      [{ text: 'OK', onPress: () => navigation.popToTop() }],
-                      { cancelable: false },
-                    )
-                  : navigation.push('SurveyQue', {
-                      questions: restQuestions,
-                      firstQuestion: false,
-                    })
-              }
+              onPress={() => formRef.current.handleSubmit()}
             />
           </View>
         </Otherwise>
