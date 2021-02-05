b544f0b260e953cc978de9897ae2c62f285d49e8
patelyashdeep <patelyashdeep3110@gmail.com>
2021-02-05 12:43:28 +0530

    ordering question

--- src/components/VKCDraggableList/index.js
+++ src/components/VKCDraggableList/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 import TextEle from '@components/TextEle';
 import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
-const VKCDraggableList = ({ data }) => {
+const VKCDraggableList = ({ data, question }) => {
   const [stateData, setStateData] = useState(data);
   const [temp, setTemp] = useState([]);
 
@@ -22,32 +22,37 @@ const VKCDraggableList = ({ data }) => {
   };
 
   return (
-    <FlatList
-      data={stateData}
-      renderItem={({ item, index }) => (
-        <RectButton
-          onPress={() => onSelect(item)}
-          style={{
-            backgroundColor: temp.some(x => x.optionId === item.optionId) ? 'red' : '#fff',
-            margin: 5,
-            padding: 10,
-            shadowColor: '#000',
-            borderRadius: 10,
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.23,
-            shadowRadius: 2.62,
-            elevation: 4,
-          }}>
-          <TextEle variant="body1">{index + 1}</TextEle>
-          <TextEle variant="body1">{item.optionName}</TextEle>
-        </RectButton>
-      )}
-      keyExtractor={item => `draggable-item-${item.optionId}`}
-      onDragEnd={({ data: updatedData }) => setStateData(updatedData)}
-    />
+    <>
+      <View>
+        <TextEle>{question}</TextEle>
+      </View>
+      <FlatList
+        data={stateData}
+        renderItem={({ item, index }) => (
+          <RectButton
+            onPress={() => onSelect(item)}
+            style={{
+              backgroundColor: temp.some(x => x.optionId === item.optionId) ? 'red' : '#fff',
+              margin: 5,
+              padding: 10,
+              shadowColor: '#000',
+              borderRadius: 10,
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.23,
+              shadowRadius: 2.62,
+              elevation: 4,
+            }}>
+            <TextEle variant="body1">{index + 1}</TextEle>
+            <TextEle variant="body1">{item.optionName}</TextEle>
+          </RectButton>
+        )}
+        keyExtractor={item => `draggable-item-${item.optionId}`}
+        onDragEnd={({ data: updatedData }) => setStateData(updatedData)}
+      />
+    </>
   );
 };
 
