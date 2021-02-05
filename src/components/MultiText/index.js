ef35a13cf28438075144dc3ab0a38ee24fb26589
imyagnesh <yagnesh.modh@gmail.com>
2021-02-05 12:08:02 +0530

    completed functionality

--- src/components/MultiText/index.js
+++ src/components/MultiText/index.js
@@ -1,6 +1,9 @@
 import VKCButton from '@components/VKCButton';
 import React, { useState } from 'react';
 import { View, TextInput } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from './styles';
 
 const MultiText = () => {
   const [textList, setTextList] = useState(['']);
@@ -8,23 +11,21 @@ const MultiText = () => {
   return (
     <View>
       <For each="ele" of={textList}>
-        <TextInput
-          style={{
-            height: 56,
-            paddingHorizontal: 24,
-            borderColor: 'black',
-            borderWidth: 2,
-            borderRadius: 32,
-            fontWeight: '500',
-            fontFamily: 'Inter-Medium',
-            fontSize: 15,
-            fontStyle: 'normal',
-            lineHeight: 18,
-            letterSpacing: 0.7,
-            textAlign: 'left',
-            marginVertical: 10,
-          }}
-        />
+        <View>
+          <TextInput
+            style={[styles.textInput]}
+            // value={value ? value[textField] : ''}
+            // placeholder={placeholder}
+          />
+          <BorderlessButton
+            style={{ position: 'absolute', right: 10, top: 18 }}
+            onPress={() => {
+              const [, ...rest] = textList;
+              setTextList(rest);
+            }}>
+            <Icon name="close" size={24} color="red" />
+          </BorderlessButton>
+        </View>
       </For>
       <VKCButton variant="fill" text="Add" onPress={() => setTextList([...textList, ''])} />
     </View>
---
+++ src/components/MultiText/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  textInput: {
+    color: '#7d7d7d',
+    fontSize: 14,
+    paddingHorizontal: 10,
+    height: 40,
+    borderColor: '#d2d2d2',
+    borderWidth: 1,
+    borderRadius: 5,
+    textAlignVertical: 'top',
+    marginVertical: 10,
+  },
+});
+
+export default styles;
