5851939ac2531e07ddc0914863c2bc834c380969
Joe Balan - Git <joebalan7@gmail.com>
2021-03-09 23:54:44 +0530

    First review with revamp of submit modal

--- src/components/SubmitModal/index.js
+++ src/components/SubmitModal/index.js
@@ -1,44 +1,45 @@
 import React from 'react';
-import { Animated,SafeAreaView, Text, ScrollView, View, Alert, Pressable,Modal,StyleSheet,Image,TouchableOpacity } from 'react-native';
+import { Animated, SafeAreaView, Text, ScrollView, View, Alert, Pressable,Modal,StyleSheet,Image,TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from '@utils/axios';
 import RNFS from 'react-native-fs';
-import TextEle from '@components/TextEle';
-const url = '/services/apexrest/SRVY_SvyCapture_API';
-const ImgAPI = '/services/apexrest/SRVY_SvyCaptureImage_API';
 import { format } from 'date-fns';
+import axios from '@utils/axios';
+import TextEle from '@components/TextEle';
+
+const captureSurveyApi = '/services/apexrest/SRVY_SvyCapture_API';
+const captureImageApi = '/services/apexrest/SRVY_SvyCaptureImage_API';
+const today = format(new Date(), 'yyyy-MM-dd');
 
 class SubmitModal extends React.Component{
-    constructor()
-    {
+    constructor() {
         super();
         this.state={
-            OuterProgressWidth:0,
-            ProgressWidth:new Animated.Value(30),
-            TotalSurvey:0,
-            TotalImages:0,
-            ImagesErrorCount:0,
-            SurveySubmited:false,
-            SubmitFinished:false,
+            outerProgressWidth: 0,
+            progressWidth: new Animated.Value(30),
+            totalSurvey: 0,
+            totalImages: 0,
+            imagesErrorCount: 0,
+            surveySubmitted: false,
+            submitFinished: false,
             Images:[],
-            ImageCount:0,
-            ErrorMsg:""
+            imageCount: 0,
+            errorMsg: ""
         }
     }
 
     componentDidMount()
     {
-        this.GetNumbers();
+        this.performSync();
     }
 
-    SetProgressBar =(val)=>{
-        Animated.timing(this.state.ProgressWidth,{
-            toValue:isNaN(val) ? 30 :val,
-            timing:5000
+    setProgressBar =(val)=>{
+        Animated.timing(this.state.progressWidth,{
+            toValue: isNaN(val) ? 30 :val,
+            timing: 5000
         }).start();
     }
 
-    RemoveImages = async()=>{
+    removeImages = async()=>{
         let AllKeys = await AsyncStorage.getAllKeys();
             AllKeys.forEach((result,index)=>{
                 if(result.includes('IMG'))
@@ -48,141 +49,87 @@ class SubmitModal extends React.Component{
             })
     }
 
-  
+    getUnsyncedDataOfCurrentDayFromStorage = async (key) => {
+        let unSyncedData = JSON.parse(await AsyncStorage.getItem(key))
+        if(!unSyncedData) return [];
 
-    GetNumbers = async () =>{
-        let Unsyncquestion = await AsyncStorage.getItem('unSyncedQuestions');
-        let ParsedSurvey = [];
-        JSON.parse(Unsyncquestion).forEach(res=>{
-            if(res.surveyDate === format(new Date(), 'yyyy-MM-dd'))
-            {
-                ParsedSurvey.push(res)
-            }
-        })
-        this.setState({TotalSurvey:ParsedSurvey.length})
-        let AllKeys = await AsyncStorage.getAllKeys();
-        let chk = AllKeys.findIndex(keys => keys.includes('IMG'));
-        console.log(chk)
-        if(chk > 0 )
-        {
-            AllKeys.forEach(async(result,index)=>{
-                if(result.includes('IMG'))
-                {
-                let Img = await AsyncStorage.getItem(result)
-                 let ParsedImages = JSON.parse(Img)
-                    ParsedImages.forEach((PImgs)=>{
-                        if(Object.keys(PImgs).length > 0)
-                        {
-                            let ImagesTemp = this.state.Images;
-                            ImagesTemp.push(PImgs);
-                            this.setState({Images:ImagesTemp},()=>{
-                                console.log(this.state.Images);
-                                this.setState({TotalImages:this.state.Images.length},()=>{
-                                    this.UploadUnsyncedSurveys(ParsedSurvey);
-                                })
-                            })
-                        }  
-                    })
+        unSyncedData = unSyncedData.filter(ud => ud.surveyDate === today && ud.syncStatus === 0)
+        if(unSyncedData.length > 0){
+            await AsyncStorage.setItem(key, JSON.stringify(unSyncedData.map(ud => ud.syncStatus = 1)));
+        }
+        return unSyncedData;
+    }
+
+    updateFailedDataBackToUnSyncedStatusInStorage = async (key) => {
+        let unSyncedData = JSON.parse(await AsyncStorage.getItem(key))
+        if(unSyncedData && unSyncedData.length > 0){
+            // get current day syncStatus = 1, update it to 0 & set it back to storage...
+            await AsyncStorage.setItem(key, JSON.stringify(unSyncedData.map(ud => {
+                if(ud.surveyDate === today && ud.syncStatus === 1) {
+                    ud.syncStatus = 0;
                 }
-            })  
+                return ud;
+            })));
         }
-        else
-        {
-            this.UploadUnsyncedSurveys(ParsedSurvey);
+    }
+
+    removeUnSyncedStatusSuccessDataInStorage = async (key) => {
+        let unSyncedData = JSON.parse(await AsyncStorage.getItem(key))
+        if(unSyncedData && unSyncedData.length > 0){
+            // get current day syncStatus = 0 & set it back to storage... this will force remove syncStatus = 1...
+            await AsyncStorage.setItem(key, JSON.stringify(unSyncedData.filter(ud => (ud.surveyDate === today && ud.syncStatus === 0))));
         }
+    }
+
+    performSync = async () =>{
+        // sync the planned visit survey...
+        this.uploadUnsyncedSurveys(getUnsyncedDataOfCurrentDayFromStorage('unSyncedQuestions'));
+
+        // sync the planned visit survey images...
+        this.uploadUnsyncedImages(getUnsyncedDataOfCurrentDayFromStorage('unSyncedImages'))
+
+        // TODO: sync the existing retailer unplanned visit survey...
+        // TODO: sync the existing retailer unplanned visit survey images...
+
+        // TODO: sync the new retailers...
+        // TODO: sync the new retailer unplanned visit survey...
+        // TODO: sync the new retailer unplanned visit survey images...
         
     }
 
 
-    UploadUnsyncedSurveys = (ParsedSurvey) => {
-        console.log("91",JSON.stringify(ParsedSurvey));
-        axios.post(url,ParsedSurvey).then(res=>{
-            console.log("89",res.data)
-            if(res.data.status === "Success")
-            {
-                this.setState({SurveySubmited:true},()=>{
-                    const {OuterProgressWidth,TotalImages,TotalSurvey} = this.state;
-                    AsyncStorage.removeItem('unSyncedQuestions');
-                    let Total = OuterProgressWidth / (TotalImages+TotalSurvey);
-                    // console.log("Total",Total,Images)
-                    let SurveyProgress = TotalSurvey * Total;
-                    this.SetProgressBar(SurveyProgress);
-                    this.UploadUnsyncedImages();
-                })
-            }
-            else
-            {
-                this.setState({ErrorMsg:JSON.stringify(res.data)},()=>{
-                    console.log("109",this.state.ErrorMsg)
-                    AsyncStorage.removeItem('unSyncedQuestions');
-                })
-                this.setState({SubmitFinished:true})
-            }
-            
-            
-        }).catch(e=>{
-            this.setState({SubmitFinished:true})
-            this.setState({ErrorMsg:e},()=>{
-                console.log("120",this.state.ErrorMsg)
+    uploadUnsyncedSurveys = (unSyncedQuestions) => {
+        if(unSyncedQuestions.length > 0){
+            axios.post(captureSurveyApi,unSyncedQuestions).then(res=>{
+                if(res.data.status === "Success") {
+                    removeUnSyncedStatusSuccessDataInStorage('unSyncedQuestions');
+                } else {
+                    updateFailedDataBackToUnSyncedStatusInStorage('unSyncedQuestions');
+                }
+            }).catch(e=>{
+                updateFailedDataBackToUnSyncedStatusInStorage('unSyncedQuestions');
+                // TODO: Specific failed unsynced status should be moved to syncStatus = 0 & success ones should be deleted...
             })
-            console.log(e)
-        })
+        }
     }
 
-    UploadUnsyncedImages=async()=>{
-        console.log(this.state.Images);
-        let SubmitImg;
-
-        if(this.state.Images.length > 0)
-        {
-            for(i = 0 ; i < this.state.Images.length ; i ++)
-            {
-            console.log("Loop",i)
-            let res =await RNFS.readFile(this.state.Images[i].uri, 'base64')
-            // console.log("98",this.state.Images[i].QId)
-            SubmitImg={
-               "surveyId": this.props.SurveyId,
-               "accountId": this.props.AccId,
-               "userId": this.props.UserId,
-               "qtnId": this.state.Images[i].QId,
-               "Sequence_No": this.state.Images[i].SqNo,
-               "imageName": this.state.Images[i].fileName,
-               "imageType": "JPG",
-               "imageBase64": res
-           }
-            
-                axios.post(ImgAPI, SubmitImg).then(res=>{
-            //   console.log("res",res)
-                    this.setState({ImageCount:this.state.ImageCount + 1},()=>{
-                   console.log("107",this.state.ImageCount)
-                   const {TotalImages,ImageCount,OuterProgressWidth,TotalSurvey} = this.state;
-                    let TotalImageCount = TotalImages - ImageCount;
-                    console.log("123",TotalImageCount)
-                    let Total = OuterProgressWidth / (TotalSurvey + TotalImageCount);
-                    console.log("Total",Total)
-                    let SurveyProgress = TotalSurvey * Total;
-                    console.log("128",OuterProgressWidth,SurveyProgress)
-                    this.SetProgressBar(SurveyProgress);
-                    console.log("115",this.state.ImageCount,this.state.Images.length)
-                    if((OuterProgressWidth - SurveyProgress) < 10)
-                    { 
-                        this.RemoveImages()
-                        this.setState({SubmitFinished:true})
-                    } 
-               })             
-            }).catch(e=>{
-               this.setState({SubmitFinished:true})
-               this.setState({ErrorMsg:e},()=>{
-                console.log("170",this.state.ErrorMsg)
-               })
+    uploadUnsyncedImages = async (unSyncedImages)=>{
+        if(unSyncedImages.length > 0) {
+            for(let i = 0; i < unSyncedImages.length; i++) {
+                let data = unSyncedImages[i];
+                data.imageBase64 = await RNFS.readFile(data.uri, 'base64')
+                delete data.uri;
+                
+                axios.post(captureImageApi, data).then(res=>{
+                    if(res.data.status === "Success") {
+                        removeUnSyncedStatusSuccessDataInStorage('unSyncedQuestions');
+                    } else {
+                        updateFailedDataBackToUnSyncedStatusInStorage('unSyncedQuestions');
+                    }
+                }).catch(e=>{
+                   
                 })
             }
-        } 
-        else
-        {
-            setTimeout(()=>{
-                this.setState({SubmitFinished:true})
-            },3000)
         }      
     }
 
@@ -191,56 +138,47 @@ class SubmitModal extends React.Component{
     render()
     {
         const animatedStyle={
-            width:this.state.ProgressWidth
+            width: this.state.progressWidth
         }
         
+        const msgWithButton = (imgSrc, displayText, errorMsg) => {
+            imgSrc = './' + imgSrc;
+            return <View style={{ width:'100%', height:'100%', alignItems:'center', padding:15 }}>
+                <Image style={{width: 125, height: 125}} source={imgSrc}/>
+                <TextEle style={{textAlign: 'center', marginTop: 25}}>{displayText}</TextEle>
+                <TextEle style={{textAlign:'center',marginTop:25}}>{errorMsg}</TextEle>
+                <TouchableOpacity onPress={() => this.props.BackToHome()} style={{ width:'100%', marginTop: 25}}>
+                    <View style={{width: '100%', height: 50, borderRadius: 10, backgroundColor: "red", alignItems: 'center', justifyContent: 'center'}}>
+                        <TextEle style={{color: 'white', fontSize: 20}}>Proceed</TextEle>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        }
+
         return(
            
             <View style={{width:'100%',height:'100%',backgroundColor:'white',marginTop:200,borderRadius:20,elevation:5,padding:10,alignItems:'center'}}>
-           { this.state.SubmitFinished ? 
-                this.state.SurveySubmited ? 
-                    this.state.ImageCount === this.state.Images.length ?
-                    <View style={{width:'100%',height:'100%',alignItems:'center',padding:15}}>
-                        <Image style={{width:125,height:125}} source={require('./checked.png')}/>
-                        <TextEle style={{textAlign:'center',marginTop:25}}>Your Survey Has Been Sucessfully Submitted.</TextEle>
-                        <TouchableOpacity onPress={()=>this.props.BackToHome()} style={{width:'100%',marginTop:25}}>
-                            <View style={{width:'100%',height:50,borderRadius:10,backgroundColor:"red",alignItems:'center',justifyContent:'center'}}>
-                                <TextEle style={{color:'white',fontSize:20}}>Proceed</TextEle>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
+            { 
+                this.state.submitFinished 
+                    ? 
+                        this.state.surveySubmitted 
+                            ? 
+                                this.state.imageCount === this.state.Images.length ?
+                                    msgWithButton('checked.png', 'Your Survey Has Been Sucessfully Submitted.', '')
+                                :
+                                    msgWithButton('warning.png', 'All Your Survey Were Submitted But Some Of Your Image Were Not Able to Upload. Please Try Again', this.state.errorMsg)                            
+                            :
+                                msgWithButton('close.png', 'Your Survey Was Not Recorded. Please Contact Support', this.state.errorMsg)// Show Error
                     :
-                    <View style={{width:'100%',height:'100%',alignItems:'center',padding:15}}>
-                        <Image style={{width:125,height:125}} source={require('./warning.png')}/>
-                        <TextEle style={{textAlign:'center',marginTop:25}}>All Your Survey Were Submitted But Some Of Your Image Were Not Able to Upload.Please Try Again</TextEle>
-                        <TextEle style={{textAlign:'center',marginTop:25}}>{typeof this.state.ErrorMsg === 'object' ? JSON.stringify(this.state.ErrorMsg) :this.state.ErrorMsg}</TextEle>
-                        <TouchableOpacity onPress={()=>this.props.BackToHome()} style={{width:'100%',marginTop:25}}>
-                            <View style={{width:'100%',height:50,borderRadius:10,backgroundColor:"red",alignItems:'center',justifyContent:'center'}}>
-                                <TextEle style={{color:'white',fontSize:20}}>Proceed</TextEle>
+                        <>
+                           <TextEle style={{marginTop:10}}>Uploading {this.state.totalSurvey} Survey and {this.state.totalImages} Images....</TextEle>
+                           <View onLayout={(event)=>{
+                                let {width}=event.nativeEvent.layout;
+                                this.setState({outerProgressWidth: parseInt(width)})
+                            }} style={{marginTop:30,width:'90%',height:25,borderRadius:10,backgroundColor:'#e0e0de'}}>
+                                <Animated.View style={[styles.progress, animatedStyle]}/>
                             </View>
-                        </TouchableOpacity>
-                    </View>
-                    :
-                    <View style={{width:'100%',height:'100%',alignItems:'center',padding:15}}>
-                        <Image style={{width:125,height:125}} source={require('./close.png')}/>
-                        <TextEle style={{textAlign:'center',marginTop:25}}>Your Survey Was Not Recorded.Please Contact Support</TextEle>
-                        <TextEle style={{textAlign:'center',marginTop:15}}>{typeof this.state.ErrorMsg === 'object' ? JSON.stringify(this.state.ErrorMsg) :this.state.ErrorMsg}</TextEle>
-                        <TouchableOpacity onPress={()=>this.props.BackToHome()} style={{width:'100%',marginTop:25}}>
-                            <View style={{width:'100%',height:50,borderRadius:10,backgroundColor:"red",alignItems:'center',justifyContent:'center'}}>
-                                <TextEle style={{color:'white',fontSize:20}}>Proceed</TextEle>
-                            </View>
-                        </TouchableOpacity>
-                    </View>// Show Error
-                    :
-                    <>
-                       <TextEle style={{marginTop:10}}>Uploading {this.state.TotalSurvey} Survey and {this.state.TotalImages} Images....</TextEle>
-                       <View onLayout={(event)=>{
-                            let {width}=event.nativeEvent.layout;
-                            this.setState({OuterProgressWidth:parseInt(width)})
-                        }} style={{marginTop:30,width:'90%',height:25,borderRadius:10,backgroundColor:'#e0e0de'}}>
-                            <Animated.View style={[styles.Progress,animatedStyle]}/>
-                        </View>
-                    </>
+                        </>
                 }
             </View>
          
@@ -249,7 +187,7 @@ class SubmitModal extends React.Component{
 }
 
 const styles = StyleSheet.create({
-    Progress:{
+    progress: {
         width:'10%',
         height:25,
         borderRadius:10,
